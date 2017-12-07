e1fa52cbff4c89e4cb2eb88b81728bde40dbe77c
Nurik <tanatov1989@mail.ru>
2017-12-07 16:52:14 +0600

    Добавьте файлы проекта.

---
+++ StructsAndEnums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nuriddin";
+            int payment = 1200000;
+           const int size = 3;
+            int[] array = new int[size];
+            Vacancies  type = Vacancies.Manager;
+            WriteLine(type);
+            Employee employee = new Employee();
+            employee.Name = name;
+            employee.Payment = payment;
+            employee.Vacansy = type;                     
+            employee.DateOfFReceivingTheGob = new int[size] ;
+           
+
+
+            
+            ReadLine();
+
+        }
+    }
+}
---
+++ StructsAndEnums/Vacancies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace StructsAndEnums
+{
+    public struct  Employee
+    {
+        
+        public string Name { get; set; }
+        public Vacancies Vacansy { get; set; }
+        public int Payment { get; set; }
+        public int[] DateOfFReceivingTheGob { get; set; }
+        public Employee(string name,int payment,int[] number)
+        {
+            const int size = 3;
+            Name = name;
+            Payment = payment;
+            DateOfFReceivingTheGob = new int[size];
+            Vacansy = Vacancies.Manager;
+            for (int i = 0; i < size; i++)
+            {
+                DateOfFReceivingTheGob[i] = number[i];
+                WriteLine(i);
+            }
+        }
+    }
+}
